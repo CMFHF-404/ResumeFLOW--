26b193db72d0c270e434c00182e42b0a7203d0f3
CMFHF <dingfan054@gmail.com>
2026-04-16 16:28:53 +0800

    feat: migrate Umami configuration to build-time environment variables and update Docker build process

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -6,6 +6,8 @@ interface ImportMetaEnv {
     readonly VITE_LOGTO_APP_ID: string
     readonly VITE_LOGTO_REDIRECT_URI: string
     readonly VITE_LOGTO_RESOURCE: string
+    readonly VITE_UMAMI_HOST: string
+    readonly VITE_UMAMI_WEBSITE_ID: string
 }
 
 interface ImportMeta {
